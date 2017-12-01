042ae75fdbaa2240e670324a1de90a6c91f05251
Joon <joon.durandt@gmail.com>
2017-12-01 08:51:57 +0000

    Code cleanup

--- src/app.tsx
+++ src/app.tsx
@@ -2,18 +2,6 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import Puzzle1 from "./puzzles/puzzle1";
-import Hello from "./hello";
-
-
-interface CompProps {
-    AName: String,
-    ANotherName: String
-}
-
-function SimpleComp(props: CompProps) {
-  return <div>Haaaai {props.AName} {props.ANotherName}</div>;
-}
-
 
 ReactDOM.render(
     <div>
